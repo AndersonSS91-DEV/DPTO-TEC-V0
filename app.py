6a8e1e5ecc93fc32c92997d7ba5cd70b03749563
Anderson S. <andersontm91@gmail.com>
2026-02-23 09:56:14 -0300

    Update app.py

--- app.py
+++ app.py
@@ -58,11 +58,11 @@ menu = st.sidebar.radio(
 
 st.title("Dashboard Operações - OPS")
 
-arquivo = st.file_uploader("Carregar base csv", type=["csv"])
+arquivo = st.file_uploader("Carregar base Excel", type=["xlsx"])
 
 if arquivo is not None:
-    df_op = pd.read_csv(arquivo, sheet_name="DPTO_TÉCNICO_TESTE")
-    df_opr = pd.read_csv(arquivo, sheet_name="OPR")
+    df_op = pd.read_excel(arquivo, sheet_name="OP")
+    df_opr = pd.read_excel(arquivo, sheet_name="OPR")
 
     df_op = df[df["Tipo"] == "OP"].copy()
     df_opr = df[df["Tipo"] == "OPR"].copy()
@@ -204,6 +204,7 @@ if arquivo is not None:
 
 else:
     st.info("Carregue a base para visualizar o dashboard.")
+
 
 
 

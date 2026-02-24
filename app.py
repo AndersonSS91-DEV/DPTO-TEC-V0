ae6e317c10d289fbc6abfa1dd713fb195f9be9e7
Anderson S. <andersontm91@gmail.com>
2026-02-24 11:07:22 -0300

    Update app.py

--- app.py
+++ app.py
@@ -220,17 +220,18 @@ if arquivo:
         col_left, col_right = st.columns([1,3])
 
         with col_left:
-    # CARD 1 - TEMPO MÉDIO
-    st.markdown(card(
-        "Tempo médio de Liberação / OP (Dias)",
-        round(tempo_medio,2) if not np.isnan(tempo_medio) else "-"
-    ), unsafe_allow_html=True)
 
-    # CARD 2 - OP's AGUARDANDO INFORMAÇÃO
-    st.markdown(card(
-        "Quantidade de OP´s Aguardando Docs e/ou Informações",
-        len(demanda)
-    ), unsafe_allow_html=True)
+            # CARD 1 - TEMPO MÉDIO
+            st.markdown(card(
+                "Tempo médio de Liberação / OP (Dias)",
+                round(tempo_medio, 2) if not np.isnan(tempo_medio) else "-"
+            ), unsafe_allow_html=True)
+
+            # CARD 2 - OP's AGUARDANDO INFORMAÇÃO
+            st.markdown(card(
+                "Quantidade de OP´s Aguardando Docs e/ou Informações",
+                len(demanda)
+            ), unsafe_allow_html=True)
 
         with col_right:
             if col_cadastro:
@@ -326,4 +327,5 @@ if arquivo:
 
 else:
     st.info("Carregue a base Excel (.xlsx) para visualizar o dashboard.")
+
 
